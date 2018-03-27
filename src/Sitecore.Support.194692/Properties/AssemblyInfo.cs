0ac1fb2d6bf290653e525d46a313db1b278a24c4
DK\VIAF <viaf@sitecore.net>
2018-03-27 08:44:20 +0300

    #Original code

---
+++ src/Sitecore.Support.194692/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.194692")]
+[assembly: AssemblyProduct("Sitecore.Support.194692")]
+[assembly: ComVisible(false)]
